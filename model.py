e72015aac35f41884e11738dfcbcc20d1bca9701
andrea-ortalda <andrea.ortalda@fcagroup.com>
2020-01-12 20:33:55 +0100

    add correct model

--- model.py
+++ model.py
@@ -1,139 +1,93 @@
-import argparse
-import base64
-from datetime import datetime
-import os
-import shutil
-
+import csv
+import cv2
 import numpy as np
-import socketio
-import eventlet
-import eventlet.wsgi
-from PIL import Image
-from flask import Flask
-from io import BytesIO
 
-from keras.models import load_model
-import h5py
-from keras import __version__ as keras_version
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.core import Dense, Activation, Flatten, Dropout
 
-sio = socketio.Server()
-app = Flask(__name__)
-model = None
-prev_image_array = None
+from keras.layers import Cropping2D
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras import backend as K
 
 
-class SimplePIController:
-    def __init__(self, Kp, Ki):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.set_point = 0.
-        self.error = 0.
-        self.integral = 0.
+lines=[]
 
-    def set_desired(self, desired):
-        self.set_point = desired
+with open('data/driving_log.csv', 'r') as f:
+    reader = csv.reader(f)
+    for line in reader:
+        lines.append(line)
+        
+images = []
+measurements = []
 
-    def update(self, measurement):
-        # proportional error
-        self.error = self.set_point - measurement
-
-        # integral error
-        self.integral += self.error
-
-        return self.Kp * self.error + self.Ki * self.integral
-
-
-controller = SimplePIController(0.1, 0.002)
-set_speed = 9
-controller.set_desired(set_speed)
-
-
-@sio.on('telemetry')
-def telemetry(sid, data):
-    if data:
-        # The current steering angle of the car
-        steering_angle = data["steering_angle"]
-        # The current throttle of the car
-        throttle = data["throttle"]
-        # The current speed of the car
-        speed = data["speed"]
-        # The current image from the center camera of the car
-        imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
-
-        throttle = controller.update(float(speed))
-
-        print(steering_angle, throttle)
-        send_control(steering_angle, throttle)
-
-        # save frame
-        if args.image_folder != '':
-            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
-            image_filename = os.path.join(args.image_folder, timestamp)
-            image.save('{}.jpg'.format(image_filename))
-    else:
-        # NOTE: DON'T EDIT THIS.
-        sio.emit('manual', data={}, skip_sid=True)
+for line in lines:
+        # Read in images from center, left and right cameras
+        center_image_path = line[0]
+        left_image_path = line[1]
+        right_image_path = line[2]
+        
+        # Read steering measurement
+        measurement_center = float(line[3])
+        measurement_left = measurement_center + 0.2
+        measurement_right = measurement_center - 0.2
+        
+        # Store paths
+        filename_center = center_image_path.split('/')[-1]
+        filename_left = left_image_path.split('/')[-1]
+        filename_right = right_image_path.split('/')[-1]
+        path_center = 'data/IMG/' + filename_center 
+        path_left = 'data/IMG/' + filename_left 
+        path_right = 'data/IMG/' + filename_right 
+        
+        # Append images and measurements
+        img_center = cv2.imread(path_center)
+        img_left = cv2.imread(path_left)
+        img_right = cv2.imread(path_right)
+        images.append(img_center)
+        images.append(img_left)
+        images.append(img_right)
+        
+        measurements.append(measurement_center)
+        measurements.append(measurement_left)
+        measurements.append(measurement_right)
+        
+        
+X_train=np.array(images)         # training images
+y_train = np.array(measurements) # labels
 
 
-@sio.on('connect')
-def connect(sid, environ):
-    print("connect ", sid)
-    send_control(0, 0)
+
+def preprocess(image):  # preprocess image
+    import tensorflow as tf
+    return tf.image.resize_images(image, (200, 66))
+
+# NVIDIA's End to end deep learning network architecture with modifications: Add a cropping layer and a lambda as preprocess and then dropout layers after each Conv2d to prevent overfitting
+# source https://classroom.udacity.com/nanodegrees/nd013/parts/168c60f1-cc92-450a-a91b-e427c326e6a7/modules/6b6c37bc-13a5-47c7-88ed-eb1fce9789a0/lessons/3fc8dd70-23b3-4f49-86eb-a8707f71f8dd/concepts/d76493fa-8061-44ab-b122-bc7fa25cd270
+# source https://arxiv.org/pdf/1604.07316v1.pdf
+
+model = Sequential()
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Conv2D(filters=24, kernel_size=(5, 5), strides=(2, 2),activation='relu'))
+model.add(Dropout(0.5))
+model.add(Conv2D(filters=36, kernel_size=(5, 5),strides=(2, 2), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Conv2D(filters=48, kernel_size=(5, 5), strides=(2, 2),activation='relu'))
+model.add(Dropout(0.5))
+model.add(Conv2D(filters=64, kernel_size=(3, 3) ,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Conv2D(filters=64, kernel_size=(3, 3),activation='relu'))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(units=100, activation='relu'))
+model.add(Dense(units=50, activation='relu'))
+model.add(Dense(units=10, activation='relu'))
+model.add(Dense(units=1))
+print(model.summary())
 
 
-def send_control(steering_angle, throttle):
-    sio.emit(
-        "steer",
-        data={
-            'steering_angle': steering_angle.__str__(),
-            'throttle': throttle.__str__()
-        },
-        skip_sid=True)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Remote Driving')
-    parser.add_argument(
-        'model',
-        type=str,
-        help='Path to model h5 file. Model should be on the same path.'
-    )
-    parser.add_argument(
-        'image_folder',
-        type=str,
-        nargs='?',
-        default='',
-        help='Path to image folder. This is where the images from the run will be saved.'
-    )
-    args = parser.parse_args()
-
-    # check that model Keras version is same as local Keras version
-    f = h5py.File(args.model, mode='r')
-    model_version = f.attrs.get('keras_version')
-    keras_version = str(keras_version).encode('utf8')
-
-    if model_version != keras_version:
-        print('You are using Keras version ', keras_version,
-              ', but the model was built using ', model_version)
-
-    model = load_model(args.model)
-
-    if args.image_folder != '':
-        print("Creating image folder at {}".format(args.image_folder))
-        if not os.path.exists(args.image_folder):
-            os.makedirs(args.image_folder)
-        else:
-            shutil.rmtree(args.image_folder)
-            os.makedirs(args.image_folder)
-        print("RECORDING THIS RUN ...")
-    else:
-        print("NOT RECORDING THIS RUN ...")
-
-    # wrap Flask application with engineio's middleware
-    app = socketio.Middleware(sio, app)
-
-    # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+model.compile(loss='mse',optimizer='adam')
+model.fit(X_train,y_train,validation_split=0.2,shuffle=True,nb_epoch=7)
+model.save('model.h5')
